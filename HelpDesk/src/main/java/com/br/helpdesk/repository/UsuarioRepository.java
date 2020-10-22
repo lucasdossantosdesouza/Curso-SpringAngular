359259abc8f0d804a4340642575f9c994d837219
lucasdossantosdesouza <lucasdossantosdesouza@gmail.com>
2020-10-22 11:53:25 -0400

    projeto helpdesk curso spring angular: cirando repositorys, services e controller

--- HelpDesk/src/main/java/com/br/helpdesk/repository/UsuarioRepository.java
+++ HelpDesk/src/main/java/com/br/helpdesk/repository/UsuarioRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsuarioRepository extends MongoRepository<Usuario,String> {
+
+    Usuario findByEmail(String email);
 }
