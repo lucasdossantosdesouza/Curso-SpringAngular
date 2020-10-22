62912198419fd52a8c73f8758219f3824c240f43
lucasdossantosdesouza <lucasdossantosdesouza@gmail.com>
2020-10-22 01:55:09 -0400

    primeira controller

--- Curso Spring Angular/src/main/java/com/br/curso/controller/FirstController.java
+++ Curso Spring Angular/src/main/java/com/br/curso/controller/FirstController.java
@@ -1,13 +1,17 @@
 package com.br.curso.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/first")
 public class FirstController {
 
-    @GetMapping(name = "/show")
-    public String showText(){
-        return "";
+    @GetMapping(value = "/show")
+    public ResponseEntity<String> showText(){
+        return new ResponseEntity<String>("Hello Word!", HttpStatus.CREATED);
     }
 }
