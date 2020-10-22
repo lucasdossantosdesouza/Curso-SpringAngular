f0a489fac80ebc08de6e6678e4ffffd000ca857e
lucasdossantosdesouza <lucasdossantosdesouza@gmail.com>
2020-10-22 12:14:15 -0400

    projeto helpdesk curso spring angular: cirando repositorys, services e controller

--- HelpDesk/src/main/java/com/br/helpdesk/repository/ChangeStatusRepository.java
+++ HelpDesk/src/main/java/com/br/helpdesk/repository/ChangeStatusRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ChangeStatusRepository extends MongoRepository<ChangeStatus, String> {
+
+    Iterable<ChangeStatus> findByTicketIdOrderByDataDesc(String ticketId);
 }
