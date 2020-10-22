e11f6626a609fa566b0bbd505e5da05d945f364e
lucasdossantosdesouza <lucasdossantosdesouza@gmail.com>
2020-10-22 13:15:04 -0400

    projeto helpdesk curso spring angular: cirando repositorys, services e controller

--- HelpDesk/src/main/java/com/br/helpdesk/service/impl/UsuarioServiceImp.java
+++ HelpDesk/src/main/java/com/br/helpdesk/service/impl/UsuarioServiceImp.java
@@ -1,12 +1,18 @@
 package com.br.helpdesk.service.impl;
 
 import com.br.helpdesk.entity.Usuario;
+import com.br.helpdesk.repository.UsuarioRepository;
 import com.br.helpdesk.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UsuarioServiceImp implements UsuarioService {
+
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
     @Override
     public Usuario findByEmail(String email) {
         return null;
