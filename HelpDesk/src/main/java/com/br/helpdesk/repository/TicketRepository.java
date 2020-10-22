97adcee1f58bb9b2a20c39928445f77523757008
lucasdossantosdesouza <lucasdossantosdesouza@gmail.com>
2020-10-22 12:04:28 -0400

    projeto helpdesk curso spring angular: cirando repositorys, services e controller

--- HelpDesk/src/main/java/com/br/helpdesk/repository/TicketRepository.java
+++ HelpDesk/src/main/java/com/br/helpdesk/repository/TicketRepository.java
@@ -1,9 +1,17 @@
 package com.br.helpdesk.repository;
 
 import com.br.helpdesk.entity.Ticket;
+import com.br.helpdesk.entity.Usuario;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TicketRepository extends MongoRepository<Ticket,String> {
+
+    Page<Ticket> findByUsuarioIdOrderByDataDesc(Pageable pageable, String userId);
+
+    Page<Ticket> findByTituloIgnoreCaseContainingAndStatusIgnoreCaseContainingAndPriorityIgnoreCaseContainingOrderByDataDesc(
+            String titulo, String status, String priority, Pageable pageable);
 }
