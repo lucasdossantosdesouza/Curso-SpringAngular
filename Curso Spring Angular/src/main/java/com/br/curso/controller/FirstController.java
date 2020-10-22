fbad65050756fa99309ceea6c72dd592bb0faba6
lucasdossantosdesouza <lucasdossantosdesouza@gmail.com>
2020-10-22 01:46:52 -0400

    Update FirstController.java

--- Curso Spring Angular/src/main/java/com/br/curso/controller/FirstController.java
+++ Curso Spring Angular/src/main/java/com/br/curso/controller/FirstController.java
@@ -1,7 +1,13 @@
 package com.br.curso.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FirstController {
+
+    @GetMapping(name = "/show")
+    public String showText(){
+        return "";
+    }
 }
